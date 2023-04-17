7210cfe547a5ff7ca6559ab75e3907b0d82f0707
mevicky9254 <vikkichoudhary6528@gmail.com>
2023-04-17 22:26:51 +0530

    calendaer added

--- scripts/gitStatsandCalendar.js
+++ scripts/gitStatsandCalendar.js
@@ -1,14 +1,14 @@
-GitHubCalendar(".react-activity-calendar", "mevicky9254");
+// GitHubCalendar(".react-activity-calendar", "mevicky9254");
   
-// or enable responsive functionality:
-GitHubCalendar(".react-activity-calendar", { responsive: true });
+// // or enable responsive functionality:
+// GitHubCalendar(".react-activity-calendar", { responsive: true });
 
-// Use a proxy
-GitHubCalendar(".react-activity-calendar", "mevicky9254", {
-   proxy (username) {
-     return fetch(`https://your-proxy.com/github?user=${mevicky9254}`)
-   }
-}).then(r => r.text())
+// // Use a proxy
+// GitHubCalendar(".react-activity-calendar", "mevicky9254", {
+//    proxy (username) {
+//      return fetch(`https://your-proxy.com/github?user=${mevicky9254}`)
+//    }
+// }).then(r => r.text())
 
 
   
